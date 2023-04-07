89b081df2580bf5222892fcc0808d89b8fce7dfa
知止 <boxoft@gmail.com>
2023-04-07 17:13:54 +0800

    feat: update manifest and improve logo replacement logic (Elon changed Twitter logo back, extension now unnecessary)

--- README.md
+++ README.md
@@ -1,8 +1,33 @@
-![marquee](./assets/marquee-promo.png)
-
 # NoDogeTweeter
 
-Revert the Doge logo and restore the original Twitter logo.
+NoDogeTweeter is a Chrome extension that reverts the Twitter logo back to its original design, replacing the Doge logo.
+
+![Marquee](./assets/marquee-promo.png)
+
+## Features
+
+- Automatically detects the Doge logo on Twitter and replaces it with the original Twitter logo
+- Preserves the original HTML structure
+
+## Installation
+
+1. Clone this repository:
+
+​	`git clone https://github.com/yourusername/NoDogeTweeter.git`
+
+2. Open Google Chrome, go to `chrome://extensions/`.
+3. Enable "Developer Mode" by toggling the switch in the top-right corner.
+4. Click the "Load unpacked" button and navigate to the cloned repository's folder, then select it.
+
+The NoDogeTweeter extension should now be installed and active in your Chrome browser.
+
+## Usage
+
+After installing the extension, simply visit Twitter, and the logo should be automatically reverted to the original one.
+
+## License
+
+This project is licensed under the MIT License. See the [LICENSE](LICENSE) file for more information.
 
 ## Acknowledgements
 
--- content.js
+++ content.js
@@ -1,7 +1,8 @@
-// Define the logo selector and the URL of the original Twitter logo.
+// Define the logo selector and the SVG of the original Twitter logo.
 const logoSelector = 'header h1[role="heading"] svg'
-const originalTwitterLogo =
-  'https://abs.twimg.com/icons/apple-touch-icon-192x192.png'
+const originalTwitterLogo = `
+<svg viewBox="0 0 24 24" aria-hidden="true" class="r-1cvl2hr r-4qtqp9 r-yyyyoo r-16y2uox r-8kz0gk r-dnmrzs r-bnwqim r-1plcrui r-lrvibr r-lrsllp"><g><path d="M23.643 4.937c-.835.37-1.732.62-2.675.733.962-.576 1.7-1.49 2.048-2.578-.9.534-1.897.922-2.958 1.13-.85-.904-2.06-1.47-3.4-1.47-2.572 0-4.658 2.086-4.658 4.66 0 .364.042.718.12 1.06-3.873-.195-7.304-2.05-9.602-4.868-.4.69-.63 1.49-.63 2.342 0 1.616.823 3.043 2.072 3.878-.764-.025-1.482-.234-2.11-.583v.06c0 2.257 1.605 4.14 3.737 4.568-.392.106-.803.162-1.227.162-.3 0-.593-.028-.877-.082.593 1.85 2.313 3.198 4.352 3.234-1.595 1.25-3.604 1.995-5.786 1.995-.376 0-.747-.022-1.112-.065 2.062 1.323 4.51 2.093 7.14 2.093 8.57 0 13.255-7.098 13.255-13.254 0-.2-.005-.402-.014-.602.91-.658 1.7-1.477 2.323-2.41z"></path></g></svg>
+`
 
 // Initialize a flag to indicate whether the logo has been replaced.
 let logoReplaced = false
@@ -18,18 +19,18 @@ const replaceLogo = () => {
   // Get the parent element of the logo.
   const logoElementWrapper = logoElement.parentNode
 
-  // Create a new image element to replace the original logo.
-  const imgElement = document.createElement('img')
-  if (logoElementWrapper && imgElement) {
-    // Set the source and size of the new image element.
-    imgElement.src = originalTwitterLogo
-    imgElement.style.width = '2rem' // Adjust the size as needed.
-    imgElement.style.height = '2rem' // Adjust the size as needed.
+  if (logoElementWrapper) {
+    // Iterate through the child elements of the logoElementWrapper.
+    for (let i = 0; i < logoElementWrapper.children.length; i++) {
+      const childElement = logoElementWrapper.children[i]
 
-    // Add the new image element to the parent element and remove the original logo.
-    logoElementWrapper.appendChild(imgElement)
-    logoElement.remove() // Remove the original SVG logo.
-    logoReplaced = true
+      // If the child element is an SVG, replace its innerHTML with the original Twitter logo SVG path.
+      if (childElement.tagName.toLowerCase() === 'svg') {
+        childElement.innerHTML = originalTwitterLogo
+        logoReplaced = true
+        break
+      }
+    }
   }
 }
 
